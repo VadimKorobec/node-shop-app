1d3e8cb37bbbba93e5acfbe0d40c8c1887c1dc15
Vadim <_s_e_t__@ukr.net>
2025-03-10 14:16:55 +0200

    add some code

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const express = require("express");
+const logger = require("morgan");
 const cors = require("cors");
 
 const app = express();
-const port = 8000;
 
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-const flavoringRoutes = require("./routes/flavorings");
+const flavoringRouter = require("./routes/flavorings");
+const booksRouter = require('./routes/books')
 
-app.use("/api/flavorings", flavoringRoutes);
+app.use("/api/flavorings", flavoringRouter);
+app.use('/api/books',booksRouter)
 
 app.use((req, res) => {
   res.status(404).json({ error: "Route not found" });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+module.exports = app;
--- controllers/brand1.js
+++ controllers/brand1.js
@@ -1,3 +1,5 @@
 exports.getAll = (req, res) => {
+  console.log(req.params)
   res.json({ message: "Brand 1" });
 };
+
